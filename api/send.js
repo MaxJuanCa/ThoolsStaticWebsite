962a8504444517f4f8ee9c06831de253cfbb2fcf
Juan Camilo Ramirez Ortiz <=>
2020-04-20 21:01:19 -0500

    TestSG5

--- api/send.js
+++ api/send.js
@@ -1,24 +1,3 @@
-const sgMail = require('@sendgrid/mail')
-
-export default async function (req, res) {
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-
-    var email = new sendgrid.Email({
-        to: 'juankamilo@gmail.com',
-        from: 'jcramirez@thoools.com',
-        subject: 'New message from ' + req.data.from,
-        html: "Name: " + req.data.name + "<br/><br/>Message: " + req.data.message
-    });
-
-    sendgrid.send(email, function (err, json) {
-        if (err) {
-            req(err)
-        } else {
-            req(null, "Your message has been submitted!")
-        }
-    });
-}
-
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const msg = {
