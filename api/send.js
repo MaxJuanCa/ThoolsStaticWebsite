ba332999002f0aa99d46cb4ceab4adac53e5ddff
Juan Camilo Ramirez Ortiz <=>
2020-04-20 21:16:59 -0500

    TestSG7

--- api/send.js
+++ api/send.js
@@ -1,38 +1,36 @@
 const sgMail = require('@sendgrid/mail')
 
 export default async function (req, res) {
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const msg = {
+        to: 'juankamilo@gmail.com',
+        from: 'jcramirez@thools.com',
+        subject: 'Sending with Twilio SendGrid is Fun',
+        text: 'and easy to do anywhere, even with Node.js',
+        html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    };
+    //ES6
+    sgMail
+        .send(msg)
+        .then(() => {}, error => {
+            console.error(error);
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-const msg = {
-    to: 'juankamilo@gmail.com',
-    from: 'jcramirez@thools.com',
-    subject: 'Sending with Twilio SendGrid is Fun',
-    text: 'and easy to do anywhere, even with Node.js',
-    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-};
-//ES6
-sgMail
-    .send(msg)
-    .then(() => {}, error => {
-        console.error(error);
+            if (error.response) {
+                console.error(error.response.body)
+            }
+        });
+    //ES8
+    (async () => {
+        try {
+            await sgMail.send(msg);
+        } catch (error) {
+            console.error(error);
 
-        if (error.response) {
-            console.error(error.response.body)
+            if (error.response) {
+                console.error(error.response.body)
+            }
         }
-    });
-//ES8
-(async () => {
-    try {
-        await sgMail.send(msg);
-    } catch (error) {
-        console.error(error);
-
-        if (error.response) {
-            console.error(error.response.body)
-        }
-    }
-})();
+    })();
 
 
 }
-
