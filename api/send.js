7d487e0584646cd29ee7f6ee4387d482b0006e04
Juan Camilo Ramirez Ortiz <=>
2020-04-20 20:58:50 -0500

    TestSG4

--- api/send.js
+++ api/send.js
@@ -18,3 +18,35 @@ export default async function (req, res) {
         }
     });
 }
+
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const msg = {
+    to: 'juankamilo@gmail.com',
+    from: 'jcramirez@thools.com',
+    subject: 'Sending with Twilio SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+};
+//ES6
+sgMail
+    .send(msg)
+    .then(() => {}, error => {
+        console.error(error);
+
+        if (error.response) {
+            console.error(error.response.body)
+        }
+    });
+//ES8
+(async () => {
+    try {
+        await sgMail.send(msg);
+    } catch (error) {
+        console.error(error);
+
+        if (error.response) {
+            console.error(error.response.body)
+        }
+    }
+})();
