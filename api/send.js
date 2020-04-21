40f8f9582a2b050f81334ec0c0711012a232007a
Juan Camilo Ramirez Ortiz <=>
2020-04-21 12:31:33 -0500

    Prueba final Sendgrid 1

--- api/send.js
+++ api/send.js
@@ -1,19 +1,32 @@
-exports.handler = function(context, event, callback) {
-    const sgMail = require("@sendgrid/mail");
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const sgMail = require('@sendgrid/mail');
 
-    const message = {
-      to: "juankamilo@gmail.com",
-      from: "jcramirez@thools.com",
-      subject: "Sending Emails with Twilio SendGrid and Twilio Server Functions is Easy",
-      text: "How simple can this be?"
+module.exports = (req, res) => {
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const msg = {
+        to: 'juankamilo@gmail.com',
+        from: 'jcramirez@thools.com',
+        subject: 'Asunto de prueba',
+        text: 'Texto de prueba',
+        html: 'Hola, escribió <strong>'+ req.query.name +'</strong> con e-mail <strong>' + req.query.from + '</strong> y dijo <strong>' + req.query.message + '</strong>'
     };
     sgMail
-      .send(message)
-      .then(() => {
-        callback(null, "Email sent successfully");
-      })
-      .catch(e => {
-        console.log(e);
-      });
-  };
+        .send(msg)
+        .then(() => {
+            location.replace("https://www.thools.com");
+        })
+        .catch(error => {
+            // Log friendly error
+            console.error(error);
+
+            if (error.response) {
+            // Extract error msg
+            const {message, code, response} = error;
+
+            // Extract response msg
+            const {headers, body} = response;
+
+            console.error(body);
+            }
+        });
+        res.status(200).send('Gracias por contactarnos '+ req.query.name);
+}
